ac882ecf1904f87edce5f315a29fbb0e0f16e8ce
akalimix <a3521110611@alumno.uttehuacan.edu.mx>
2022-10-14 00:25:27 -0500

    Primera Version

---
+++ src/components/TotalProyectos.vue
@@ -0,0 +1,51 @@
+<template>
+    
+      <div class="row">
+        <div class="col-6 my-2">
+            <h3>
+                Total Proyectos : {{numeroProyectos}}
+              </h3>
+        </div>
+        <div class="col-6 text-end">
+            <button @click="limpiarData" class="btn btn-warning">Limpiar proyecto</button>
+        </div>
+      </div>
+
+    <div class="table-responsive">
+        <table class="table table-dark table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Proyecto</th>
+                    <th>Tipo</th>
+                    <th>Urgente</th>
+                    <th>Completado</th>
+                    <th>Borrar</th>
+                </tr>
+            </thead>
+            <tbody v-for="(proyecto,index) in proyectos" :key="index">
+                <tr >
+                    <td>{{index + 1}}</td>
+                    <td>{{proyecto.proyecto}}</td>
+                    <td>{{proyecto.tipo}}</td>
+                    <td @click="cambiarEstado(proyecto,'urgente')" :class="proyecto.urgente ?
+                     'bg-success' : 'bg-danger'">{{proyecto.urgente ? "Si" : "No"}}</td>
+
+                  <td @click="cambiarEstado(proyecto,'completado')" :class="proyecto.completado ?
+                     'bg-success' : 'bg-danger'">{{proyecto.completado ? "Completado" : "Incompleto"}}</td>
+
+                    <td @click="borrar(proyecto,'borrar')" :class="proyecto.borrar ?
+                     'bg-success' : 'bg-danger'">{{proyecto.borrar ?  "Borrar" : "Borrar"}}</td>
+                </tr>
+            </tbody>
+     
+        </table>
+      </div>
+</template>
+
+        <script>
+        export default {
+            props: [ 'numeroProyectos', 'proyectos', 'cambiarEstado', 'limpiarData'],
+           
+        }
+        </script>
